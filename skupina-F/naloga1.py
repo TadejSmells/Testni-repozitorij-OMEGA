ad37b3e1932f31bfa8c74e722cdce8b3851b97f7
Maja2806 <maja.lukic2@student.um.si>
2026-03-15 18:00:43 +0100

    Dodana naloga1.py v mapo skupina-F

---
+++ skupina-F/naloga1.py
@@ -0,0 +1,10 @@
+# Program za izračun ploščine pravokotnika
+
+print("Izračun ploščine pravokotnika")
+
+dolzina = float(input("Vnesi dolžino: "))
+sirina = float(input("Vnesi širino: "))
+
+ploscina = dolzina * sirina
+
+print("Ploščina pravokotnika je:", ploscina)
